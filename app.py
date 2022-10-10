22690ceb91f0479e72c537a8ba4bf915b0f540e7
Shogo Ogami <sg.tao.so@gmail.com>
2022-10-10 19:19:13 +0900

    add public folder for simple web app as client

--- app.py
+++ app.py
@@ -6,9 +6,10 @@
 # | GET   | /api/v1/users/123   | ユーザ情報の取得 |
 # 
 import os
+import os.path
 import json
 import sqlite3
-from flask import Flask, request
+from flask import Flask, request, Response, send_from_directory
 import pandas as pd
 #
 app = Flask(__name__)
@@ -72,6 +73,46 @@ def search_user():
     #
     conn.close()
     return result
+#
+# temporal web service
+# refer web-dev-qa-db-ja.com
+def public_dir():  # pragma: no cover
+    exec_dir = os.path.abspath(os.path.dirname(__file__))
+    # print(exec_dir)
+    return os.path.join(exec_dir, 'public/')
+#
+def get_public_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(public_dir(), filename)
+        # This should not be so non-obvious
+        # - use read alternatively render_template, send_from
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+#
+@app.route('/')
+def index():
+    content = get_public_file('index.html')
+    return Response(content, mimetype="text/html")
+#
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(public_dir(),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+#
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+    }
+    complete_path = os.path.join(public_dir(), path)
+    ext = os.path.splitext(path)[1]
+    # print('request file extention is ', ext)
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_public_file(complete_path)
+    return Response(content, mimetype=mimetype)
 #
 # run after setup
 def main():
